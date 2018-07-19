ce125251351f5e9714c39a23e33c3ca633bf88ce
WQRice <wq2@rice.edu>
2018-07-19 16:40:30 -0500

    Update 2018-07-19T21:40:08.341Z

---
+++ module/parse2Front.js
@@ -0,0 +1,29 @@
+'use strict';
+var ejs  = require('ejs');
+var fs   = require('fs-extra');
+// var parser = require('./XMLParser');
+
+module.exports=function(packagePath,datas){
+
+    var datasobj = {'dataArray':datas};
+
+     var template = fs.readFileSync(packagePath+'/frontendTemplate/index_html_template.ejs', 'utf-8');
+     var file = ejs.render ( template , datasobj );
+     fs.outputFileSync("OutputProject/src/main/resources/static/index.html", file, 'utf8');
+ 
+     var template = fs.readFileSync(packagePath+'/frontendTemplate/appController_js_template.ejs', 'utf-8');
+     var file = ejs.render ( template , datasobj );
+     fs.outputFileSync("OutputProject/src/main/resources/static/js/appController.js", file, 'utf8');
+ 
+ 
+    for (var i in datas) {
+      var template = fs.readFileSync(packagePath+'/frontendTemplate/view_html_template.ejs', 'utf-8');
+     var file = ejs.render ( template , datas[i] );
+     fs.outputFileSync("OutputProject/src/main/resources/static/js/views/"+datas[i].CLASSNAME+".html", file, 'utf8');
+   
+     var template = fs.readFileSync(packagePath+'/frontendTemplate/viewModel_js_template.ejs', 'utf-8');
+    var file = ejs.render ( template , datas[i] );
+   fs.outputFileSync("OutputProject/src/main/resources/static/js/viewModels/"+datas[i].CLASSNAME+".js", file, 'utf8');
+  }
+
+}
---
+++ module/parse2Java.js
@@ -0,0 +1,41 @@
+'use strict';
+var ejs = require('ejs');
+var fs = require('fs-extra');
+var caseConvert=require("./CaseConvert");
+
+module.exports = function (packagePath,datas) {
+
+    for (var i in datas) {
+        //produce controller
+        var template = fs.readFileSync(packagePath+'/backendTemplate/controller.ejs', 'utf-8');
+        var file = ejs.render(template, datas[i]);
+        fs.outputFileSync("OutputProject/src/main/java/OITWeb/" + datas[i].PACKAGENAME + "/controller/" + datas[i].CLASSNAME + 'Controller.java', file, 'utf8');
+
+        //produce entity class
+        var template = fs.readFileSync(packagePath+'/backendTemplate/entity.ejs', 'utf-8');
+        var file = ejs.render(template, datas[i]);
+        fs.outputFileSync("OutputProject/src/main/java/OITWeb/" + datas[i].PACKAGENAME + "/model/" + datas[i].CLASSNAME + '.java', file, 'utf8');
+
+        //produce repository class
+        var template = fs.readFileSync(packagePath+'/backendTemplate/repository.ejs', 'utf-8');
+        var file = ejs.render(template, datas[i]);
+        fs.outputFileSync("OutputProject/src/main/java/OITWeb/" + datas[i].PACKAGENAME + "/repository/" + datas[i].CLASSNAME + 'Repository.java', file, 'utf8');
+    }
+
+
+    if (datas.length > 0) {
+        //produce exception class
+        var template = fs.readFileSync(packagePath+'/backendTemplate/exception.ejs', 'utf-8');
+        var file = ejs.render(template, datas[0]);
+        fs.outputFileSync("OutputProject/src/main/java/OITWeb/" + datas[0].PACKAGENAME + "/exception/" + 'ResourceNotFoundException.java', file, 'utf8');
+
+        //produce entry class
+        var template = fs.readFileSync(packagePath+'/backendTemplate/application.ejs', 'utf-8');
+        var file = ejs.render(template, datas[0]);
+        fs.outputFileSync("OutputProject/src/main/java/OITWeb/" + caseConvert.upFirst(datas[0].PACKAGENAME)+ 'Application.java', file, 'utf8');
+
+        var template = fs.readFileSync(packagePath+'/backendTemplate/repositoryAdapter.ejs', 'utf-8');
+        var file = ejs.render(template, datas[0]);
+        fs.outputFileSync("OutputProject/src/main/java/OITWeb/" + datas[0].PACKAGENAME + "/repository/" + 'RepositoryAdapter.java', file, 'utf8');
+    }
+}
