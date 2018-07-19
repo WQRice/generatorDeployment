aaedd0465efb9513856bc7c5cbc06ce613efc601
WQRice <31909355+WQRice@users.noreply.github.com>
2018-07-19 16:04:41 -0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
  
 gulp.task('deploy', function() {
-  return gulp.src('./generator-starter/*')
+  return gulp.src('./**/*')
     .pipe(ghPages({'remoteUrl':'https://github.com/WQRice/generatorDeplyment.git'}));
 });
